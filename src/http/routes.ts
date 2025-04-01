ba16f58e1323b6214e6c12e2c6649c6ccd4386bd
Matheus Valandro <msvalandro@proton.me>
2025-04-01 19:28:54 +0100

    feat: add profile endpoint and route
    - Create new profile controller with basic structure
    - Add GET /me route to fetch user profile

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,8 +2,10 @@ import { FastifyInstance } from 'fastify'
 
 import { authenticate } from './controllers/authenticate'
 import { register } from './controllers/register'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.get('/me', profile)
 }
